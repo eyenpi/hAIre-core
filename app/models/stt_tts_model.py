31e53a39138c3daaa099d5dc2b12011d2274df39
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2024-10-22 07:54:01 +0200

    Implement STT service

---
+++ app/models/stt_tts_model.py
@@ -0,0 +1,11 @@
+import whisper
+
+class STT:
+    model = None
+
+    def __init__(self):
+        self.model = whisper.load_model('tiny')
+
+    def execute(self, audio):
+        return self.model.transcribe(audio)['text']
+
