5a02acf97bfdccb846696bb11ffb5013452cfe8b
Ali Nabipour <alinabipour01@gmail.com>
2024-10-23 17:47:53 +0200

    add cors and mock cv

--- app/api/v1/routes/cv_extraction.py
+++ app/api/v1/routes/cv_extraction.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, UploadFile, File, Depends
+from fastapi import APIRouter, UploadFile, File
 from app.utils.singleton import AgentSingleton
 from app.utils.file_reader import get_hr_questions
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
@@ -13,4 +14,31 @@ async def cv_extraction(cv_file: UploadFile = File(...)):
     updated_questions = get_hr_questions()
     AgentSingleton.update_instance(updated_questions)
 
-    return {"message": "CV details extracted successfully!"}
+    return JSONResponse(
+        status_code=200,
+        content={
+            "name": "Hosein",
+            "email": "hoseinmirhoseini64@gmail.com",
+            "work_experiences": [
+                {
+                    "position": "Software Engineer",
+                    "company": "Scalapay",
+                    "from_to": "2021-01-01 to Present",
+                    "description": "Software Engineer at Scalapay",
+                },
+                {
+                    "position": "Frontend Developer",
+                    "company": "Apple",
+                    "from_to": "2020-01-01 to 2021-01-01",
+                    "description": "Frontend Developer at Scalapay",
+                },
+            ],
+            "educations": [
+                {
+                    "degree": "Bachelor of Computer Science",
+                    "institution": "Shiraz University",
+                    "from_to": "2017-01-01 to 2021-09-01",
+                },
+            ],
+        },
+    )
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.routes import (
     speech_to_text,
     text_to_speech,
@@ -14,6 +15,15 @@ app = FastAPI(
     description="An end-to-end system for CV screening, interviewing using LLMs, and evaluation.",
 )
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Can specify specific domains instead of "*"
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow specific HTTP methods like 'GET', 'POST' etc.
+    allow_headers=["*"],  # Allow specific headers
+)
+
 # Including the individual routes
 app.include_router(speech_to_text.router, prefix="/api/v1/stt", tags=["Speech-to-Text"])
 app.include_router(text_to_speech.router, prefix="/api/v1/tts", tags=["Text-to-Speech"])
@@ -23,7 +33,6 @@ app.include_router(hr_panel.router, prefix="/api/v1/hr-panel", tags=["HR Panel"]
 app.include_router(
     pseudonymize.router, prefix="/api/v1/pseudonymize", tags=["Pseudonymize"]
 )
-
 
 if __name__ == "__main__":
     import uvicorn
