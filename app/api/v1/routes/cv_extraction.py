a7cab7fe0cdafe167e6067805dd4389f3d7c0036
Ali Nabipour <alinabipour01@gmail.com>
2024-10-24 10:34:16 +0200

    use mock for now

--- app/api/v1/routes/cv_extraction.py
+++ app/api/v1/routes/cv_extraction.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter, UploadFile, File
 
 from app.services.cv_extraction_service import extract_text_from_cv, segment_cv
 from app.services.pseudonymize import DataProcessor
+from app.utils.file_reader import get_hr_questions
+from fastapi.responses import JSONResponse
+from app.utils.singleton import AgentSingleton
+
 
 router = APIRouter()
 
@@ -9,7 +13,7 @@ router = APIRouter()
 @router.post("/extract")
 async def cv_extraction(cv_file: UploadFile = File(...)):
     pdf_bytes = await cv_file.read()
-    text = extract_text_from_cv(pdf_bytes)
+    # text = extract_text_from_cv(pdf_bytes)
 
     # dp = DataProcessor()
 
@@ -17,40 +21,37 @@ async def cv_extraction(cv_file: UploadFile = File(...)):
     # segmented_text = segment_cv(pseudonymized_text)
     #
     # dipseudonymized_text = dp.depseudonymize_text(segmented_text, pseudonymized_entity_dict)
-    segmented_text = segment_cv(text)
-    return {'message': segmented_text}
+    # segmented_text = segment_cv(text)
+    # return {'message': segmented_text}
 
+    updated_questions = get_hr_questions()
+    AgentSingleton.update_instance(updated_questions)
 
-    #####################################################################
-    # Update the agent's questions
-    # updated_questions = get_hr_questions()
-    # AgentSingleton.update_instance(updated_questions)
-    #
-    # return JSONResponse(
-    #     status_code=200,
-    #     content={
-    #         "name": "Hosein",
-    #         "email": "hoseinmirhoseini64@gmail.com",
-    #         "work_experiences": [
-    #             {
-    #                 "position": "Software Engineer",
-    #                 "company": "Scalapay",
-    #                 "from_to": "2021-01-01 to Present",
-    #                 "description": "Software Engineer at Scalapay",
-    #             },
-    #             {
-    #                 "position": "Frontend Developer",
-    #                 "company": "Apple",
-    #                 "from_to": "2020-01-01 to 2021-01-01",
-    #                 "description": "Frontend Developer at Scalapay",
-    #             },
-    #         ],
-    #         "educations": [
-    #             {
-    #                 "degree": "Bachelor of Computer Science",
-    #                 "institution": "Shiraz University",
-    #                 "from_to": "2017-01-01 to 2021-09-01",
-    #             },
-    #         ],
-    #     },
-    # )
+    return JSONResponse(
+        status_code=200,
+        content={
+            "name": "Hosein",
+            "email": "hoseinmirhoseini64@gmail.com",
+            "work_experiences": [
+                {
+                    "position": "Software Engineer",
+                    "company": "Scalapay",
+                    "from_to": "2021-01-01 to Present",
+                    "description": "Software Engineer at Scalapay",
+                },
+                {
+                    "position": "Frontend Developer",
+                    "company": "Apple",
+                    "from_to": "2020-01-01 to 2021-01-01",
+                    "description": "Frontend Developer at Scalapay",
+                },
+            ],
+            "educations": [
+                {
+                    "degree": "Bachelor of Computer Science",
+                    "institution": "Shiraz University",
+                    "from_to": "2017-01-01 to 2021-09-01",
+                },
+            ],
+        },
+    )
